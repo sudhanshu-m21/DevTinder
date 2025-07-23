fe6570aa66e425a3edb73a42915a54860cb6752d
sudhanshu-m21 <sudhanshumishra106@gmail.com>
2025-07-23 13:14:14 +0530

    chanbde database code

--- Devtinder Backend/src/config/database.js
+++ Devtinder Backend/src/config/database.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
 // require("dotenv").config();
 const connectDB = async () => {
-  await mongoose.connect(process.env.DB_URL);
+  // await mongoose.connect(process.env.DB_URL);
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+  }
 };
 module.exports = connectDB();
