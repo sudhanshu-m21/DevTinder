8b66ec02eb0ef85a697946e843ed76756999e224
sudhanshu-m21 <sudhanshumishra106@gmail.com>
2025-07-23 13:32:07 +0530

    changed some database connection code

--- Devtinder Backend/src/config/database.js
+++ Devtinder Backend/src/config/database.js
@@ -1,14 +1,6 @@
 const mongoose = require("mongoose");
-// require("dotenv").config();
+require("dotenv").config();
 const connectDB = async () => {
-  // await mongoose.connect(process.env.DB_URL);
-  try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  } catch (err) {
-    console.error("MongoDB connection failed:", err);
-  }
+  await mongoose.connect(process.env.DB_URL);
 };
 module.exports = connectDB();
