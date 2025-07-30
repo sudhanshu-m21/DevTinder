a86404126e7417fc28b673cad2570ab62f25ec0e
sudhanshu-m21 <sudhanshumishra106@gmail.com>
2025-07-30 12:02:18 +0530

    socket url updated

--- Devtinder Frontend/src/utils/socket.js
+++ Devtinder Frontend/src/utils/socket.js
@@ -3,5 +3,8 @@ import { BASE_URL } from "../constant";
 
 export const createSocketConnection = () => {
   if (location.hostname == "localhost") return io(BASE_URL);
-  else return io("/", { path: "/api/socket.io" });
+  else
+    return io("/", {
+      path: "https://devtinder-backend-wuv9.onrender.com/socket.io",
+    });
 };
